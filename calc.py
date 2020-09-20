07d40c7e326ea3e29bc13a9448e7ff9e68cd26b5
HYEJINKIM <un123k@naver.com>
2020-09-20 20:33:23 +0900

    update calc.py

--- calc.py
+++ calc.py
@@ -1,8 +1,11 @@
 # add program
 
+def sum_function(n1, n2):
+	return n1+n2
+
 num1 = int(input("input number1 "))
 num2 = int(input("input number2 "))
 
-sum = num1 + num2
+sum = sum_function(num1, num2)
 
 print(num1, "+", num2, "=", sum)
